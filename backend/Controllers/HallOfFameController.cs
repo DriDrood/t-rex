9c70b3e6725807f1f4e17dd5a20bc69119c927e8
Petr Watson-Jones <98212945+PetrWatsonJones@users.noreply.github.com>
2022-07-09 17:33:25 +0200

    #16 HallOfFameController update

--- backend/Controllers/HallOfFameController.cs
+++ backend/Controllers/HallOfFameController.cs
@@ -4,17 +4,17 @@ using Microsoft.AspNetCore.Mvc;
 using trex.Database;
 
 namespace trex.Controllers;
-
-
-public class HallOfFameController
+[ApiController]
+[Route("api/[controller]")]
+public class HallOfFameController : Controller
 {
     static List<PlayerResult> hallOfFame = new List<PlayerResult>();
-    
+    [HttpPost("[action]")]
     public void AddResult(PlayerResult result)
     {
         hallOfFame.Add(result);
     }
-    
+    [HttpGet("[action]")]
     public List<PlayerResult> GetTop(int count)
     {
         return hallOfFame.OrderByDescending(r => r.Score).Take(count).ToList();
