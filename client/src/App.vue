4955bee5b78e5cbac615e0d2d7e81bfc5ba9604a
HonzaKubita <honza.kubita@gmail.com>
2022-07-09 21:52:01 +0200

    Removed Nav bar

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,30 +1,16 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+<RouterView/>
 </template>
 
+<script>
+
+</script>
+
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+
+body, button, input, textarea {
+  font-family: 'Press Start 2P', cursive;
 }
 
-nav {
-  padding: 30px;
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
 </style>
