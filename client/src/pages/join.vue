9e0723a8d7911eaf568d183aa6e72777df001096
manLuke <kachatomas@gmail.com>
2022-07-10 01:08:22 +0200

    join

--- client/src/pages/join.vue
+++ client/src/pages/join.vue
@@ -1,22 +1,19 @@
 <template>
-  <div class="join">
-    <div class="join-container">
-      <h2>
-        <div class="join-input">
-          <span></span>
-          <p>Lobby id:</p>
-          <input type="text" pattern="[0-9a-f]*" placeholder="ad5aa1bf-7b5c-4ef2-bea7-ffbe2aad50f6" />
-          <span></span>
-          <p>Nickname:</p>
-          <input type="text" placeholder="nickname" pattern="\w{3,15}" />
-        </div>
-        <div class="join-btn">
-          <button @click="this.$router.push('/')">Back</button>
-          <button @click="this.$router.push('')">Join</button>
-        </div>
-      </h2>
+  <div class="join-main">
+    <h2>Join</h2>
+    <div class="join-input">
+      <span class="primary-errMsg">{{ errMsg }}</span>
+      <p>Lobby id:</p>
+      <input type="text" pattern="[0-9a-f]*" placeholder="ad5aa1bf-7b5c-4ef2-bea7-ffbe2aad50f6" />
+      <p>Nickname:</p>
+      <input type="text" placeholder="nickname" pattern="\w{3,15}" />
+    </div>
+    <div class="join-btn">
+      <button class="primary-button" @click="this.$router.push('/')">Back</button>
+      <button class="primary-button" @click="this.$router.push('')">Join</button>
     </div>
   </div>
+
 </template>
 
 <script>
@@ -27,16 +24,52 @@ export default {
       user: {
         nickname: '',
       },
-    };
+      errMsg: "Name is not valid"
+    }
   },
+  computed: {
+
+  }
 };
 </script>
 
 <style lang="scss">
-.join {
+.join-main {
   display: flex;
   justify-content: center;
   align-items: center;
+  flex-direction: column;
   height: 90%;
+
+  h2 {
+    font-size: 60px;
+    margin: 50px;
+  }
+}
+
+.join-input {
+  display: grid;
+
+  input {
+    margin: 5px 0;
+    width: 700px;
+    height: 40px;
+    border-radius: 5px;
+    border: 1px solid #ccc;
+    padding: 10px;
+    border-radius: 0;
+    border: solid black 4px;
+  }
+
+  span {
+    width: 100%;
+    text-align: center;
+  }
+}
+
+.join-btn {
+  display: flex;
+  gap: 100px;
+  margin-top: 50px;
 }
 </style>
