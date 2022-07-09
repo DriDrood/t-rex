4b43923ef32ee3ecac7f4923e6409e55eb800092
Squirrelegend <jakubwj2@gmail.com>
2022-07-09 21:09:03 +0200

    player id

--- server/Models/Player.cs
+++ server/Models/Player.cs
@@ -2,11 +2,13 @@ namespace trex.Models;
 
 public class Player
 {
+    public Guid id;
     public string name { get; set; }
     public int score { get; set; }
     public Player(string _name)
     {
         this.name = _name;
         this.score = 0;
+        this.id = Guid.NewGuid();
     }
 }
