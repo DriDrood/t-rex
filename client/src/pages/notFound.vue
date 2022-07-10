6a5b464e025d03d82e025276c4add3f47c04ce67
HonzaKubita <honza.kubita@gmail.com>
2022-07-10 09:59:54 +0200

    404 Page

---
+++ client/src/pages/notFound.vue
@@ -0,0 +1,30 @@
+<template>
+<div class="main-404">
+  <h1 class="title2-primary title-404">404 Not found</h1>
+  <img src="/assets/images/404.png" alt="404"/>
+  <button @click="this.$router.push('/')" class="primary-button">Home</button>
+</div>
+</template>
+
+<script>
+</script>
+
+<style>
+.main-404 {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+.title-404 {
+  animation-name: floating;
+  animation-duration: 3s;
+  animation-iteration-count: infinite;
+  animation-timing-function: ease-in-out;
+}
+@keyframes floating {
+  0% { transform: translate(0,  0px); }
+  50%  { transform: translate(0, 15px); }
+  100%   { transform: translate(0, -0px); }   
+}
+</style>
