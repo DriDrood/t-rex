a4793a6fd3fd55315ef8272e1a1384fc32981a7f
HonzaKubita <honza.kubita@gmail.com>
2022-07-11 18:19:33 +0200

    404 Page style

--- client/src/pages/notFound.vue
+++ client/src/pages/notFound.vue
@@ -1,12 +1,20 @@
 <template>
 <div class="main-404">
-  <h1 class="title2-primary title-404">404 Not found</h1>
+  <h1 class="title2-primary title-404">404 Not Found!</h1>
   <img src="/assets/images/404.png" alt="404"/>
   <button @click="this.$router.push('/')" class="btn primary-button">Home</button>
 </div>
 </template>
 
 <script>
+
+export default {
+  name: 'notFound',
+  data: () => ({
+    message: ''
+  }),
+}
+
 </script>
 
 <style>
@@ -15,16 +23,5 @@
   flex-direction: column;
   align-items: center;
   justify-content: center;
-}
-.title-404 {
-  animation-name: floating;
-  animation-duration: 3s;
-  animation-iteration-count: infinite;
-  animation-timing-function: ease-in-out;
-}
-@keyframes floating {
-  0% { transform: translate(0,  0px); }
-  50%  { transform: translate(0, 15px); }
-  100%   { transform: translate(0, -0px); }   
 }
 </style>
