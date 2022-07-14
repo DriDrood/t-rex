047a15f3d7a14e418139171dce08b45234853fd2
HonzaKubita <honza.kubita@gmail.com>
2022-07-15 00:44:17 +0200

    Array method

--- client/src/game-src/modules/input.js
+++ client/src/game-src/modules/input.js
@@ -2,7 +2,7 @@ export default {
   pressedKeys: [], // Array of currently pressed keys
   init() {
     document.onkeydown = ({key}) => { // When key is pressed add it to array
-      this.pressedKeys.append(key);
+      this.pressedKeys.push(key);
     }
     document.onkeyup = ({key}) => { // When key is released remove it from array
       this.pressedKeys.splice(pressedKeys.indexOf(key), 1);
