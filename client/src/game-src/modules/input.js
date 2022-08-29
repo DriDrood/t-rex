2f06f5a3730fbb402f0a90f86f820001147a700a
HonzaKubita <honza.kubita@gmail.com>
2022-08-29 11:17:57 +0200

    Sending client input to server

--- client/src/game-src/modules/input.js
+++ client/src/game-src/modules/input.js
@@ -1,11 +1,24 @@
+import signalR from '@/signalR.js'
 export default {
   pressedKeys: [], // Array of currently pressed keys
   init() {
     document.onkeydown = ({key}) => { // When key is pressed add it to array
       this.pressedKeys.push(key);
+      console.log(key);
+      this.send();
     }
     document.onkeyup = ({key}) => { // When key is released remove it from array
       this.pressedKeys.splice(pressedKeys.indexOf(key), 1);
+      this.send();
     }
+  },
+  send() {
+    let pressedArrows = {
+      up: this.pressedKeys.includes('ArrowUp'),
+      down: this.pressedKeys.includes('ArrowDown'),
+      left: this.pressedKeys.includes('ArrowLeft'),
+      right: this.pressedKeys.includes('ArrowRight')
+    }
+    signalR.invoke('KeyPressed', pressedArrows);
   }
 }
